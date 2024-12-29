85953afbbfdc14c0c30d5770c9723169cda02ae2
eric tang <eric.x.tang@gmail.com>
2024-12-29 01:09:23 -0500

    add cover video

--- app/page.tsx
+++ app/page.tsx
@@ -32,6 +32,7 @@ export default function HomePage() {
   };
 
   useEffect(() => {
+    handleStreamSwitch("https://vod-cdn.lp-playback.studio/raw/jxf4iblf6wlsyor6526t4tcmtmqa/catalyst-vod-com/hls/8a77r9bted6od9oz/index.m3u8");
     fetchStatuses();
     const interval = setInterval(fetchStatuses, 5000);
     return () => clearInterval(interval);
